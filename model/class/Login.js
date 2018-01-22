85b319e1f3c104a775d5c7c172ee78a249caaf94
Maquiavelo98 <doconesa@hotmail.com>
2018-01-22 09:16:11 +0100

    cambios en login

--- model/class/Login.js
+++ model/class/Login.js
@@ -1,14 +1,10 @@
 class Login{
     constructor(id,nick,pass){
         var _id =id;
-        var _nick = nick;
         var _pass = pass;
 
         this.getId = function(){
             return _id;
-        };
-        this.getNick = function () {
-            return _nick;
         };
         this.getPass = function () {
             return _pass;
@@ -16,9 +12,6 @@ class Login{
 
         this.setId = function (id) {
             _id = id;
-        };
-        this.setNick = function (nick) {
-          _nick = nick
         };
         this.setPass = function (pass) {
             _pass = pass;
