e1606b9d2520a4c34bb3ab641193e2ee1b5ec7a5
Maquiavelo98 <doconesa@hotmail.com>
2018-01-28 02:00:11 +0100

    pruebas

--- view/js/forms.js
+++ view/js/forms.js
@@ -62,11 +62,11 @@ function rellFormPermiso() {
         "fechaLlegada": "2018-01-27",
         "motivo": "prenatales",
         "jornada": {
-            "1": {
-                "diaInicio": "",
+            "a": {
+                "diaInicio": "1998-10-05",
                 "diaFin": ""
             },
-            "2": {
+            "b": {
                 "diaInicio": "",
                 "horaInicio": "",
                 "diaFin": "",
@@ -74,35 +74,35 @@ function rellFormPermiso() {
             }
         },
         "horario": {
-            "1": {
-                "dia": "2018-01-",
+            "a": {
+                "dia": "2018-01-01",
                 "hora": "",
-                "curso": "",
-                "asignatura": "",
-                "sustituto": ""
+                "curso": "asa",
+                "asignatura": "asa",
+                "sustituto": "asa"
             },
-            "2": {
+            "b": {
                 "dia": "",
                 "hora": "",
                 "curso": "",
                 "asignatura": "",
                 "sustituto": ""
             },
-            "3": {
+            "c": {
                 "dia": "",
                 "hora": "",
                 "curso": "",
                 "asignatura": "",
                 "sustituto": ""
             },
-            "4": {
+            "d": {
                 "dia": "",
                 "hora": "",
                 "curso": "",
                 "asignatura": "",
                 "sustituto": ""
             },
-            "5": {
+            "e": {
                 "dia": "",
                 "hora": "",
                 "curso": "",
@@ -121,45 +121,79 @@ function rellFormPermiso() {
 
     switch (peticion.motivo) {
         case "matrimonio":
-            $("#motivo-matrimonio").attr("checked", true)
+            $("#motivo-matrimonio").attr("checked", true);
             break;
         case "prenatales":
-            $("#motivo-prenatales").attr("checked", true)
+            $("#motivo-prenatales").attr("checked", true);
             break;
         case "lactancia":
-            $("#motivo-lactancia").attr("checked", true)
+            $("#motivo-lactancia").attr("checked", true);
             break;
         case "embarazo":
-            $("#motivo-embarazo").attr("checked", true)
+            $("#motivo-embarazo").attr("checked", true);
             break;
         case "permiso-medico":
-            $("#motivo-permiso-medico").attr("checked", true)
+            $("#motivo-permiso-medico").attr("checked", true);
             break;
         case "enfermedad":
-            $("#motivo-enfermedad").attr("checked", true)
+            $("#motivo-enfermedad").attr("checked", true);
             break;
         case "pruebas":
-            $("#motivo-pruebas").attr("checked", true)
+            $("#motivo-pruebas").attr("checked", true);
             break;
         case "traslado":
-            $("#motivo-traslado").attr("checked", true)
+            $("#motivo-traslado").attr("checked", true);
             break;
         case "deber-inexcusable":
-            $("#motivo-deber-inexcusable").attr("checked", true)
+            $("#motivo-deber-inexcusable").attr("checked", true);
             break;
         case "funciones-representativas":
-            $("#motivo-funciones-representativas").attr("checked", true)
+            $("#motivo-funciones-representativas").attr("checked", true);
             break;
     }
 
-    switch (peticion.jornada) {
-        case 1:
-            break;
-        case 2:
-            break;
-    }
+    //jornada
+    $("#completa-fecha-desde").val(peticion.jornada.a.diaInicio);
+    $("#completa-fecha-hasta").val(peticion.jornada.a.diaFin);
 
+    $("#incompleta-fecha-desde").val(peticion.jornada.b.diaInicio);
+    $("#incompleta-fecha-hasta").val(peticion.jornada.b.diaFin);
+    $("#incompleta-hora-desde").val(peticion.jornada.b.horaInicio);
+    $("#incompleta-hora-hasta").val(peticion.jornada.b.horaFin);
 
+    // horario
+    $("#horario-dia-1").val(peticion.horario.a.dia);
+    $("#horario-hora-1").val(peticion.horario.a.hora);
+    $("#horario-curso-1").val(peticion.horario.a.curso);
+    $("#horario-asignaruta-1").val(peticion.horario.a.asignatura);
+    $("#horario-profesor-1").val(peticion.horario.a.sustituto);
+
+    $("#horario-dia-2").val(peticion.horario.b.dia);
+    $("#horario-hora-2").val(peticion.horario.b.hora);
+    $("#horario-curso-2").val(peticion.horario.b.curso);
+    $("#horario-asignaruta-2").val(peticion.horario.b.asignatura);
+    $("#horario-profesor-2").val(peticion.horario.b.sustituto);
+
+    $("#horario-dia-3").val(peticion.horario.c.dia);
+    $("#horario-hora-3").val(peticion.horario.c.hora);
+    $("#horario-curso-3").val(peticion.horario.c.curso);
+    $("#horario-asignaruta-3").val(peticion.horario.c.asignatura);
+    $("#horario-profesor-3").val(peticion.horario.c.sustituto);
+
+    $("#horario-dia-4").val(peticion.horario.d.dia);
+    $("#horario-hora-4").val(peticion.horario.d.hora);
+    $("#horario-curso-4").val(peticion.horario.d.curso);
+    $("#horario-asignaruta-4").val(peticion.horario.d.asignatura);
+    $("#horario-profesor-4").val(peticion.horario.d.sustituto);
+
+    $("#horario-dia-5").val(peticion.horario.e.dia);
+    $("#horario-hora-5").val(peticion.horario.e.hora);
+    $("#horario-curso-5").val(peticion.horario.e.curso);
+    $("#horario-asignaruta-5").val(peticion.horario.e.asignatura);
+    $("#horario-profesor-5").val(peticion.horario.e.sustituto);
+
+    //comentarios
+    $("#documentacion-observaciones").val(peticion.comentarios);
 
 
 }
