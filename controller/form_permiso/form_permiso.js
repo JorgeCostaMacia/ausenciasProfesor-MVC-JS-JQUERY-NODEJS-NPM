1eea3771e5d066533ffd157be16d7940a6810e4d
JorgeCostaMacia <jorgemovilistico@gmail.com>
2018-01-28 19:32:40 +0100

    update

--- controller/form_permiso/form_permiso.js
+++ controller/form_permiso/form_permiso.js
@@ -5,24 +5,12 @@
 function evalGenPermiso(event) {
     msjClean();
 
-    let id = "";
-
-    let idusuario = gestor.getLocal().id;
-
-    let nombreSolicitante = $("#donya").val();
-
-    let cola = event.target.value;
-
-    let fechaActual = new Date();
-    let mes = fechaActual.getMonth() * 1 + 1;
-    if (mes < 10) mes = "" + "0" + mes;
-    fechaActual = fechaActual.getFullYear() + "-" + (mes) + "-" + fechaActual.getDate();
-
-
-    let motivo = $("input[name=motivo-permiso]:checked").val();
-
-
-    let jornada = {
+    let peticion = {};
+    peticion["idUsuario"] = gestor.getLocal()["id"];
+    peticion["nombreSolicitante"] = $("#donya").val();
+    peticion["cola"] = event.target.value;
+    peticion["motivo"] = $("input[name=motivo-permiso]:checked").val();
+    peticion["jornada"] = {
         "completa": {
             "diaInicio": $("#completa-fecha-desde").val(),
             "diaFin": $("#completa-fecha-hasta").val(),
@@ -34,24 +22,30 @@ function evalGenPermiso(event) {
             "horaFin": $("#incompleta-hora-hasta").val(),
         }
     };
-
-    let horario = {};
+    peticion["horario"] = {};
     for(let i = 1; i < 9; i++){
-        let dia = $("#horario-dia-" + i).val();
-        let hora = $("#horario-hora-" + i).val();
-        let curso = $("#horario-curso-" + i).val();
-        let asignatura = $("#horario-asignatura-" + i).val();
-        let sustituto = $("#horario-profesor-" + i).val();
-
-        horario[i] = { "dia": dia, "hora": hora, "curso": curso, "asignatura": asignatura, "sustituto": sustituto };
+        peticion["horario"][i] = {
+            "dia": $("#horario-dia-" + i).val(),
+            "hora": $("#horario-hora-" + i).val(),
+            "curso": $("#horario-curso-" + i).val(),
+            "asignatura": $("#horario-asignatura-" + i).val(),
+            "sustituto": $("#horario-profesor-" + i).val(),
+        };
     }
+    peticion["observaciones"] = $("#documentacion-observaciones").val();
+    peticion["fechaFirma"] = $("#firma-anyo").val()+"-"+$("#firma-mes").val()+"-"+$("#firma-dia").val();
+    peticion["firma"] = $("#firma-firma").val();
+    peticion["comentarios"] = [];
+    peticion["anexos"] = [];
 
-    let observaciones = $("#documentacion-observaciones").val();
-    let fechaFirma = $("#firma-anyo").val()+"-"+$("#firma-mes").val()+"-"+$("#firma-dia").val();
-    let firma = $("#firma-firma").val();
-    let comentarios = [];
-    let anexos = [];
 
-    peticionManager.addPeticion(idusuario, cola, nombreSolicitante, fechaActual, fechaActual, motivo, JSON.stringify(jornada), JSON.stringify(horario), observaciones, fechaFirma, firma, JSON.stringify(comentarios), JSON.stringify(anexos));
 
+
+    let fechaActual = new Date();
+    let mes = fechaActual.getMonth() * 1 + 1;
+    if (mes < 10) mes = "" + "0" + mes;
+    fechaActual = fechaActual.getFullYear() + "-" + (mes) + "-" + fechaActual.getDate();
+
+
+    peticionManager.addPeticion(JSON.stringify(peticion));
 }
