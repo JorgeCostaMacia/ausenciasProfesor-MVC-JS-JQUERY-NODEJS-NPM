6a7a1c174e9b3ea4c83ce0d36809f06b1ccbd2fa
Maquiavelo98 <doconesa@hotmail.com>
2018-01-28 01:32:16 +0100

    cambios

--- view/js/forms.js
+++ view/js/forms.js
@@ -1,11 +1,11 @@
 "use strict";
 
-function injectNombreCompletoDisable(nombre){
-    $("#buscador-texto").attr('value',nombre);
-    $("#buscador-texto").attr('disabled','disabled');
+function injectNombreCompletoDisable(nombre) {
+    $("#buscador-texto").attr('value', nombre);
+    $("#buscador-texto").attr('disabled', 'disabled');
 }
 
-function injectCaption(text){
+function injectCaption(text) {
     $("#caption").append(text);
 }
 
@@ -28,7 +28,7 @@ function injectPeticiones(peticiones) {
     }
 }
 
-function injectRegistors(registros){
+function injectRegistors(registros) {
     for (let i = 0; i < registros.length; i++) {
         $("#tbody").append(
             '<tr id="tr-' + registros[i].getId() + '">' +
@@ -38,8 +38,8 @@ function injectRegistors(registros){
             '<td>' + registros[i].getNivel() + '</td>' +
             '<td>' +
             '<div class="d-flex">' +
-            '<button id="aceptar-' +  registros[i].getId() + '" class="btn btn-success btn-sm text-uppercase mr-3">Aceptar</button>' +
-            '<button id="denegar-' +  registros[i].getId() + '" class="btn btn-danger btn-sm text-uppercase">Rechazar</button>' +
+            '<button id="aceptar-' + registros[i].getId() + '" class="btn btn-success btn-sm text-uppercase mr-3">Aceptar</button>' +
+            '<button id="denegar-' + registros[i].getId() + '" class="btn btn-danger btn-sm text-uppercase">Rechazar</button>' +
             '</div>' +
             '</td>' +
             '</tr>'
@@ -47,59 +47,119 @@ function injectRegistors(registros){
     }
 }
 
-function delTrRegistro(idTr){ $('#' + idTr).remove(); }
+function delTrRegistro(idTr) {
+    $('#' + idTr).remove();
+}
 
-function rellFormPermiso(obj){
-    let obj = {
-      "id": 1,
-      "idUsuario": "admino",
-      "cola": "genPermiso",
-      "nombreSolicitante": "jorge costa macia",
-      "fechaCreacion": "2018-01-01",
-      "fechaLlegada": "2018-01-27",
-      "motivo": "",
-      "jornada": "",
-      "desde": "",
-      "hasta": "",
-      "horario": {
-        "1": {
-          "dia": "",
-          "hora": "",
-          "curso": "",
-          "asignatura": "",
-          "sustituto": ""
+function rellFormPermiso() {
+
+    let peticion = {
+        "id": 1,
+        "idUsuario": "jorge",
+        "cola": "genPermiso",
+        "nombreSolicitante": "jorge costa macia",
+        "fechaCreacion": "2018-01-01",
+        "fechaLlegada": "2018-01-27",
+        "motivo": "prenatales",
+        "jornada": {
+            "1": {
+                "diaInicio": "",
+                "diaFin": ""
+            },
+            "2": {
+                "diaInicio": "",
+                "horaInicio": "",
+                "diaFin": "",
+                "horaFin": ""
+            }
         },
-        "2": {
-          "dia": "",
-          "hora": "",
-          "curso": "",
-          "asignatura": "",
-          "sustituto": ""
+        "horario": {
+            "1": {
+                "dia": "2018-01-",
+                "hora": "",
+                "curso": "",
+                "asignatura": "",
+                "sustituto": ""
+            },
+            "2": {
+                "dia": "",
+                "hora": "",
+                "curso": "",
+                "asignatura": "",
+                "sustituto": ""
+            },
+            "3": {
+                "dia": "",
+                "hora": "",
+                "curso": "",
+                "asignatura": "",
+                "sustituto": ""
+            },
+            "4": {
+                "dia": "",
+                "hora": "",
+                "curso": "",
+                "asignatura": "",
+                "sustituto": ""
+            },
+            "5": {
+                "dia": "",
+                "hora": "",
+                "curso": "",
+                "asignatura": "",
+                "sustituto": ""
+            }
         },
-        "3": {
-          "dia": "",
-          "hora": "",
-          "curso": "",
-          "asignatura": "",
-          "sustituto": ""
-        },
-        "4": {
-          "dia": "",
-          "hora": "",
-          "curso": "",
-          "asignatura": "",
-          "sustituto": ""
-        },
-        "5": {
-          "dia": "",
-          "hora": "",
-          "curso": "",
-          "asignatura": "",
-          "sustituto": ""
-        }
-      },
-      "comentarios": [],
-      "anexos": []
+        "comentarios": "",
+        "anexos": []
+    };
+
+
+    let don = $("#ddña");
+    don.val(peticion.nombreSolicitante);
+    don.attr("disabled", true);
+
+    switch (peticion.motivo) {
+        case "matrimonio":
+            $("#motivo-matrimonio").attr("checked", true)
+            break;
+        case "prenatales":
+            $("#motivo-prenatales").attr("checked", true)
+            break;
+        case "lactancia":
+            $("#motivo-lactancia").attr("checked", true)
+            break;
+        case "embarazo":
+            $("#motivo-embarazo").attr("checked", true)
+            break;
+        case "permiso-medico":
+            $("#motivo-permiso-medico").attr("checked", true)
+            break;
+        case "enfermedad":
+            $("#motivo-enfermedad").attr("checked", true)
+            break;
+        case "pruebas":
+            $("#motivo-pruebas").attr("checked", true)
+            break;
+        case "traslado":
+            $("#motivo-traslado").attr("checked", true)
+            break;
+        case "deber-inexcusable":
+            $("#motivo-deber-inexcusable").attr("checked", true)
+            break;
+        case "funciones-representativas":
+            $("#motivo-funciones-representativas").attr("checked", true)
+            break;
     }
+
+    switch (peticion.jornada) {
+        case 1:
+            break;
+        case 2:
+            break;
+    }
+
+
+
 
 }
