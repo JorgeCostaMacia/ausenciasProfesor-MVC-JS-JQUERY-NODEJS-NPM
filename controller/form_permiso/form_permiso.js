d02d2773f28048c27853da6ff5e7352ab85931a0
Maquiavelo98 <doconesa@hotmail.com>
2018-01-29 08:13:07 +0100

    correccion errores merge

--- controller/form_permiso/form_permiso.js
+++ controller/form_permiso/form_permiso.js
@@ -7,6 +7,21 @@ function evalGenPermiso(event) {
 
     let permiso = gestor.getPeticiones()[0];
 
+    let peticion = {};
+    peticion["idUsuario"] = gestor.getLocal()["id"];
+    peticion["cola"] = event.target.value;
+    peticion["nombreSolicitante"] = $("#donya").val();
+
+    if(peticion.getCola() == "penAutorizarPermiso"){
+        peticion["comentarios"] = permiso.getComentarios();
+        peticion["anexos"] = permiso.getAnexos();
+        peticion["fechaCreacion"] = permiso.getFechaCreacion();
+    }
+    else{
+        peticion["comentarios"] = [];
+        peticion["anexos"] = [];
+        peticion["fechaCreacion"] = gestor.getDate();
+    }
 
     peticion["fechaLlegada"] = gestor.getDate();
     peticion["motivo"] = $("input[name=motivo-permiso]:checked").val();
@@ -45,25 +60,54 @@ function evalGenPermiso(event) {
     log["colaDestino"] = event.target.value;
 
     gestor.addLogs(new Log(log));
->>>>>>> master
 
-    peticionManager.addPeticion(peticion, 'addLog');
-}
+    peticionManager.addPeticion(peticion, 'addLog');msjClean();
 
-<<<<<<< HEAD
+    let permiso = gestor.getPeticiones()[0];
 
-    let jornada = {
+    let peticion = {};
+    peticion["idUsuario"] = gestor.getLocal()["id"];
+    peticion["cola"] = event.target.value;
+    peticion["nombreSolicitante"] = $("#donya").val();
+
+    if(peticion.getCola() == "penAutorizarPermiso"){
+        peticion["comentarios"] = permiso.getComentarios();
+        peticion["anexos"] = permiso.getAnexos();
+        peticion["fechaCreacion"] = permiso.getFechaCreacion();
+    }
+    else{
+        peticion["comentarios"] = [];
+        peticion["anexos"] = [];
+        peticion["fechaCreacion"] = gestor.getDate();
+    }
+
+    peticion["fechaLlegada"] = gestor.getDate();
+    peticion["motivo"] = $("input[name=motivo-permiso]:checked").val();
+    peticion["jornada"] = {
         "completa": {
-            "diaInicio": "" + $("#completa-fecha-desde").val(),
-            "diaFin": "" + $("#completa-fecha-hasta").val(),
+            "diaInicio": $("#completa-fecha-desde").val(),
+            "diaFin": $("#completa-fecha-hasta").val(),
         },
         "parcial": {
-            "diaInicio": "" + $("#incompleta-fecha-desde").val(),
-            "diaFin": "" + $("#incompleta-fecha-hasta").val(),
-            "horaInicio": "" + $("#incompleta-hora-desde").val(),
-            "horaFin": "" + $("#incompleta-hora-hasta").val(),
+            "diaInicio": $("#incompleta-fecha-desde").val(),
+            "diaFin": $("#incompleta-fecha-hasta").val(),
+            "horaInicio": $("#incompleta-hora-desde").val(),
+            "horaFin": $("#incompleta-hora-hasta").val(),
         }
     };
+    peticion["horario"] = {};
+    for(let i = 1; i < 9; i++){
+        peticion["horario"][i] = {
+            "dia": $("#horario-dia-" + i).val(),
+            "hora": $("#horario-hora-" + i).val(),
+            "curso": $("#horario-curso-" + i).val(),
+            "asignatura": $("#horario-asignatura-" + i).val(),
+            "sustituto": $("#horario-profesor-" + i).val(),
+        };
+    }
+    peticion["observaciones"] = $("#documentacion-observaciones").val();
+    peticion["fechaFirma"] = $("#firma-anyo").val()+"-"+$("#firma-mes").val()+"-"+$("#firma-dia").val();
+    peticion["firma"] = $("#firma-firma").val();
 
     let horario = {
         "1": {
@@ -136,16 +180,7 @@ function evalGenPermiso(event) {
 function addLog(ressult){
     msjClean();
 
-    let logObjetc = gestor.getLogs()[0];
->>>>>>> master
+    gestor.addLogs(new Log(log));
 
-    let log = {};
-    log["idUsuario"] = logObjetc.getIdUsuario();
-    log["nombre"] = logObjetc.getNombre();
-    log["fecha"] = logObjetc.getFecha();
-    log["hora"] = logObjetc.getHora();
-    log["colaInicio"] = logObjetc.getColaInicio();
-    log["colaDestino"] = logObjetc.getColaDestino();
-
-    logManager.addLog(log, 'changePageIninicio');
+    peticionManager.addPeticion(peticion, 'addLog');
 }
