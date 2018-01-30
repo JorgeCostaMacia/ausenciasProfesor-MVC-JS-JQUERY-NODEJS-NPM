190b7bd129032630d54c889f191a1114398478f9
Maquiavelo98 <doconesa@hotmail.com>
2018-01-30 12:27:19 +0100

    arreglado subida anexos

--- controller/form_justificante/anexos.js
+++ controller/form_justificante/anexos.js
@@ -1,40 +1,44 @@
 "use strict";
 
-function evalExistAnexo(ressult){
-    if($("#adjunto-documentacion-obervaciones").val() != "" ||
+function evalExistAnexo(ressult) {
+    if ($("#adjunto-documentacion-obervaciones").val() != "" ||
         $("#adjunto-documentacion-obervaciones").val() != null ||
-        $("#adjunto-documentacion-obervaciones").val() != "null"){
+        $("#adjunto-documentacion-obervaciones").val() != "null") {
 
         let ruta = $("#adjunto-documentacion-obervaciones").val().split(".");
 
-        if(ruta[ruta.length - 1 ] = "pdf"){
+        if (ruta[ruta.length - 1] = "pdf") {
             anexosManager.getAnexos("id=" + gestor.getPeticiones()[0].getIdPeticion(), 'anyadirAnexo');
         }
-        else { addLog(""); }
+        else {
+            addLog("");
+        }
     }
-    else { addLog(""); }
+    else {
+        addLog("");
+    }
 }
 
-function anyadirAnexo(ressult){
+function anyadirAnexo(ressult) {
     let anexo = new Anexo(ressult[0]);
-    let anexoFile = readFile();
-    anexo.addAnexos(anexoFile);
-    console.log(anexoFile);
-    let anexoJSON = { "id": anexo.getId(), "anexos": anexo.getAnexos() };
-
-    anexosManager.updateAnexos(anexoJSON, gestor.getPeticiones()[0].getIdPeticion(), "addLog");
+    readFile(anexo);
 }
 
-function readFile(){
-    let ressult = "";
+function readFile(anexo) {
     let file = document.querySelector('input[type=file]').files[0];
     let reader = new FileReader();
 
-    if (file) { reader.readAsDataURL(file); }
+    if (file) {
+        reader.readAsDataURL(file);
+    }
 
     reader.onloadend = function () {
-        ressult = reader.result;
-        console.log(ressult);
-        return ressult;
+        grabarAnexo(anexo,reader.result);
     }
+}
+
+function grabarAnexo(anexo,anexoFile) {
+    anexo.addAnexos(anexoFile);
+    let anexoJSON = {"id": anexo.getId(), "anexos": anexo.getAnexos()};
+    anexosManager.updateAnexos(anexoJSON, gestor.getPeticiones()[0].getIdPeticion(), "addLog");
 }
