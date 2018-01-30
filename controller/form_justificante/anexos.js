7cec849837fb40ff58a51388c21db38623b3b3ac
JorgeCostaMacia <jorgemovilistico@gmail.com>
2018-01-30 16:30:24 +0100

    update errores

--- controller/form_justificante/anexos.js
+++ controller/form_justificante/anexos.js
@@ -1,49 +1,34 @@
 "use strict";
 
-function evalExistAnexo(ressult) {
-    if ($("#adjunto-documentacion-obervaciones").val() != "" ||
-        $("#adjunto-documentacion-obervaciones").val() != null ||
-        $("#adjunto-documentacion-obervaciones").val() != "null") {
+function evalExistAnexo(ressult){
+    if($("#adjunto-documentacion-obervaciones").val() != ""){
 
         let ruta = $("#adjunto-documentacion-obervaciones").val().split(".");
 
-<<<<<<< HEAD
         if(ruta[ruta.length - 1 ] = "pdf"){
-            anexosManager.getAnexos(gestor.getPeticiones()[0].getIdPeticion(), 'anyadirAnexo');
-=======
-        if (ruta[ruta.length - 1] = "pdf") {
-            anexosManager.getAnexos("id=" + gestor.getPeticiones()[0].getIdPeticion(), 'anyadirAnexo');
->>>>>>> 190b7bd129032630d54c889f191a1114398478f9
+            anexosManager.getAnexos(gestor.getPeticiones()[0].getIdPeticion(), 'readFile');
         }
-        else {
-            addLog("");
-        }
+        else { addLog(""); }
     }
-    else {
-        addLog("");
-    }
+    else { addLog(""); }
 }
 
-function anyadirAnexo(ressult) {
-    let anexo = new Anexo(ressult[0]);
-    readFile(anexo);
-}
-
-function readFile(anexo) {
+function readFile(ressult) {
+    let anexo = new Anexo(ressult);
     let file = document.querySelector('input[type=file]').files[0];
     let reader = new FileReader();
 
-    if (file) {
-        reader.readAsDataURL(file);
-    }
+    if (file) { reader.readAsDataURL(file); }
 
     reader.onloadend = function () {
-        grabarAnexo(anexo,reader.result);
+        let fileAnex = reader.result;
+        anexo.addAnexos(fileAnex);
+        grabarAnexo(anexo);
     }
 }
 
-function grabarAnexo(anexo,anexoFile) {
-    anexo.addAnexos(anexoFile);
-    let anexoJSON = {"id": anexo.getId(), "anexos": anexo.getAnexos()};
+function grabarAnexo(anexo) {
+    let anexoJSON = { "anexos": anexo.getAnexos()};
+    console.log(anexoJSON);
     anexosManager.updateAnexos(anexoJSON, gestor.getPeticiones()[0].getIdPeticion(), "addLog");
 }
